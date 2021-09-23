9113236b36e23f4649458d9114cf24a2bf137d6c
Gustavo Molina <gustavo.molina17@yahoo.com.br>
2021-09-23 09:08:04 -0300

    Adicionando Exemplo 2 de for

---
+++ Aula 06/For2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+	
+	
+	int contador; // variável de controle do loop
+	
+	for (contador = 10;contador>=0;contador--){
+		
+		printf("%d", contador);
+			
+	}
+	
+	return 0;
+	
+}
