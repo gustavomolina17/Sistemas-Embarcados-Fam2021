fbf03d07b0136abded6a237728092482165e5c81
Gustavo Molina <gustavo.molina17@yahoo.com.br>
2021-09-23 09:11:30 -0300

    Adicionando o 3º Exemplo de for

---
+++ Aula 06/For3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+	
+	int i; // Variável de controle do loop
+	
+	for( i = 1; i<=4; i++){
+		
+		printf("Aprendendo a linguagem C\n");
+		
+	}
+	
+	return 0;
+		
+}
