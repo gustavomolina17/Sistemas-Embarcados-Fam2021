6c4f9ec4499aa2bfb59c97e4ca2f1220efe2c5aa
Gustavo Molina <gustavo.molina17@yahoo.com.br>
2021-09-23 08:33:47 -0300

    Adicionando Exemplo While

---
+++ Aula 06/While.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (void){
+	
+	int contador = 1; // declarando e inicializando a variável de controle
+	
+	while (contador <=10){  // Testando a condição
+	
+	printf("%d", contador); // Comando a ser executado dentro do laço
+	
+	contador ++; // atualizando a variável de controle
+	
+}
+	
+	return 0;
+		
+}
